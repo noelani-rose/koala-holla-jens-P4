9da2b737d04e44575758384e935d27914b4e5a58
AlinaTrukhina <alina.trukhina@gmail.com>
2022-10-06 12:00:08 -0500

    add radio button

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -16,11 +16,11 @@ function setupClickListeners() {
     // NOT WORKING YET :(
     // using a test object
     let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
+      name: $('#nameIn').val(),
+      age: $('#ageIn').val(),
+      gender: $('#genderIn').val(),
+      readyForTransfer: $('#readyForTransferIn').val(),
+      notes: $('#notesIn').val()
     };
     // call saveKoala with the new obejct
     saveKoala( koalaToSend );
@@ -36,5 +36,5 @@ function getKoalas(){
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
   // ajax call to server to get koalas
- 
-}
+  
+} // end saveKoala
