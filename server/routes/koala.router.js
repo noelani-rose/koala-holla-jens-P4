85376e59581f3a34ba3e1ccd6df3506b68b40ac6
Noelani West <noelani.rose14@gmail.com>
2022-10-06 11:57:02 -0500

    create router get request and pool query

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const koalaRouter = express.Router();
+const pool = require('../modules/pool');
 
 // DB CONNECTION
 
 
 // GET
+koalaRouter.get('/', (req, res) =>{
+    console.log('in koala router GET koalas');
+    pool.query(`SELECT * FROM "koalla_holla";`)
 
+        .then((dbRes) =>{
+            res.send(dbRes.rows);
+        })
+        .catch((err) =>{
+            console.log('getting koalas failed...', err); 
+            res.sendStatus(500);
+        }); 
+});
 
 // POST
 
