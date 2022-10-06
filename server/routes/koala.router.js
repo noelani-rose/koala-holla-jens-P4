b9afe285b39d7c66a8edc35f4465786a88c3bfbe
Crystal Redmon <cato2848@gmail.com>
2022-10-06 13:46:11 -0500

    put complete

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,4 +1,6 @@
+const { Router } = require('express');
 const express = require('express');
+const { Pool } = require('pg');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
@@ -11,6 +13,34 @@ const koalaRouter = express.Router();
 
 
 // PUT
+
+ Router.put('/:id', (req, res)=>{
+    const readyForTransfer = req.body.transfer;
+
+    let sqlText = `UPDATE "koalla"
+                    SET "transfer" = TRUE
+                    WHERE "id" = 1;`;
+
+        Pool.query(sqlText, [$1])
+
+        .then((response)=>{
+            res.sendStatus(200);
+        })
+        .catch((error)=>{
+            console.log('Error, change to transfer failed', error);
+        });
+
+
+
+ })
+
+
+
+
+
+
+
+
 
 
 // DELETE
